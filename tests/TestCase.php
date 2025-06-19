a8677e4e67339ab7b262d1f9920f6cf17c22c744
MarcelWeidum <9413586+MarcelWeidum@users.noreply.github.com>
2025-06-19 15:17:02 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -14,9 +14,9 @@ use Filament\Tables\TablesServiceProvider;
 use Filament\Widgets\WidgetsServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Livewire\LivewireServiceProvider;
+use MarcelWeidum\ExpirationNoticePlugin\ExpirationNoticePluginServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
-use MarcelWeidum\ExpirationNoticePlugin\ExpirationNoticePluginServiceProvider;
 
 class TestCase extends Orchestra
 {
