e58d0ff617a7d2e8294587b445b0b56e30926578
MarcelWeidum <marcelweidum@live.nl>
2025-06-19 19:32:50 +0200

    Remove boilerplate

--- src/ExpirationNoticeServiceProvider.php
+++ src/ExpirationNoticeServiceProvider.php
@@ -4,15 +4,10 @@ declare(strict_types=1);
 
 namespace MarcelWeidum\ExpirationNoticePlugin;
 
-use Filament\Support\Assets\AlpineComponent;
 use Filament\Support\Assets\Asset;
-use Filament\Support\Assets\Css;
 use Filament\Support\Assets\Js;
 use Filament\Support\Facades\FilamentAsset;
-use Filament\Support\Facades\FilamentIcon;
-use Illuminate\Filesystem\Filesystem;
 use Livewire\Features\SupportTesting\Testable;
-use MarcelWeidum\ExpirationNoticePlugin\Commands\ExpirationNoticePluginCommand;
 use MarcelWeidum\ExpirationNoticePlugin\Testing\TestsExpirationNoticePlugin;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
@@ -26,18 +21,10 @@ final class ExpirationNoticeServiceProvider extends PackageServiceProvider
 
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package->name(self::$name)
-            ->hasCommands($this->getCommands())
             ->hasInstallCommand(function (InstallCommand $command) {
                 $command
                     ->publishConfigFile()
-                    ->publishMigrations()
-                    ->askToRunMigrations()
                     ->askToStarRepoOnGitHub('marcelweidum/filament-expiration-notice');
             });
 
@@ -47,16 +34,8 @@ final class ExpirationNoticeServiceProvider extends PackageServiceProvider
             $package->hasConfigFile();
         }
 
-        if (file_exists($package->basePath('/../database/migrations'))) {
-            $package->hasMigrations($this->getMigrations());
-        }
-
         if (file_exists($package->basePath('/../resources/lang'))) {
             $package->hasTranslations();
-        }
-
-        if (file_exists($package->basePath('/../resources/views'))) {
-            $package->hasViews(self::$viewNamespace);
         }
     }
 
@@ -69,23 +48,6 @@ final class ExpirationNoticeServiceProvider extends PackageServiceProvider
             $this->getAssets(),
             $this->getAssetPackageName()
         );
-
-        FilamentAsset::registerScriptData(
-            $this->getScriptData(),
-            $this->getAssetPackageName()
-        );
-
-        // Icon Registration
-        FilamentIcon::register($this->getIcons());
-
-        // Handle Stubs
-        if (app()->runningInConsole()) {
-            foreach (app(Filesystem::class)->files(__DIR__.'/../stubs/') as $file) {
-                $this->publishes([
-                    $file->getRealPath() => base_path("stubs/filament-expiration-notice/{$file->getFilename()}"),
-                ], 'filament-expiration-notice-stubs');
-            }
-        }
 
         // Testing
         Testable::mixin(new TestsExpirationNoticePlugin);
@@ -102,53 +64,7 @@ final class ExpirationNoticeServiceProvider extends PackageServiceProvider
     protected function getAssets(): array
     {
         return [
-            // AlpineComponent::make('filament-expiration-notice', __DIR__ . '/../resources/dist/components/filament-expiration-notice.js'),
-            // Css::make('filament-expiration-notice-styles', __DIR__ . '/../resources/dist/filament-expiration-notice.css'),
             Js::make('filament-expiration-notice-scripts', __DIR__.'/../resources/dist/filament-expiration-notice.js'),
-        ];
-    }
-
-    /**
-     * @return array<class-string>
-     */
-    protected function getCommands(): array
-    {
-        return [
-            ExpirationNoticePluginCommand::class,
-        ];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getIcons(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getRoutes(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string, mixed>
-     */
-    protected function getScriptData(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getMigrations(): array
-    {
-        return [
-            'create_filament-expiration-notice_table',
         ];
     }
 }
