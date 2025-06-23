71c1c5fcb81900644464de496374848fdb50a81e
CodeWithDennis <personal.elsinga@gmail.com>
2025-06-23 19:42:24 +0200

    Add expiration notice translations for DE and NL languages

---
+++ resources/lang/nl/expiration-notice.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'heading' => 'Sessie verlopen',
+    'description' => 'Uw pagina-sessie is verlopen door inactiviteit. Vernieuw de pagina om door te gaan.',
+    'button' => 'Vernieuw pagina',
+];
